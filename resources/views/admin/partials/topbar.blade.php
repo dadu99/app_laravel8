83c2400a86010cc461fc3ce145228a0c298742ce
Darius Baciu <baciu.darius01@gmail.com>
2023-07-13 17:18:29 +0300

    logout functionality & some changes

--- resources/views/admin/partials/topbar.blade.php
+++ resources/views/admin/partials/topbar.blade.php
@@ -13,12 +13,24 @@
             <!-- Navbar-->
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{auth()->user()->name}}
+                        <i class="fas fa-user fa-fw"></i>
+                    </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Settings</a></li>
-                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                        <li><a class="dropdown-item" href="#!">Profile</a></li>
+                        <li><a class="dropdown-item" href="#!">My pages</a></li>
+                        <li><hr class="dropdown-divider"/></li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <li>
+                                    <a class="dropdown-item" href="#"
+                                        onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                        Logout
+                                    </a>
+                                </li>
+                            </form>
                     </ul>
                 </li>
             </ul>
--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -4,8 +4,12 @@
         @include("admin.partials.topbar")
         <div id="layoutSidenav">
             @include('admin.partials.sidenav')
-        
-            @yield('content-users')
+            <div id="layoutSidenav_content">
+                <main>
+                    @yield('content-users')
+                    @yield('content')
+                </main>
+            </div>
         </div>
        @include('admin.partials.scripts')
     </body>
