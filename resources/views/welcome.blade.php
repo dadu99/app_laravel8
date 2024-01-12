3658eacc35355e148470b5d55a9c2640a14564e0
Darius Baciu <baciu.darius01@gmail.com>
2024-01-12 12:54:46 +0200

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -408,15 +408,17 @@
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
-            <div class="hidden fixed top-0 text-center text-light bg-dark px-6 py-4 sm:block">
+            <div class="hidden fixed top-130 text-center bg-dark px-6 py-4 sm:block">
                 @auth
                     <a href="{{ url('/dashboard') }}"
-                        class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                        class="badge-secondary badge-secondary text-lg text-gray-700 dark:text-gray-500">Dashboard</a>
                 @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700">Log in</a>
+                    <a href="{{ route('login') }}" class=" btn btn-info text-lg text-gray-700 dark:text-white-500"
+                        role="button">Log in</a>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500">Register</a>
+                        <a href="{{ route('register') }}"
+                            class="badge-secondary ml-4 text-lg text-gray-700 dark:text-white-500">Register</a>
                     @endif
                 @endauth
             </div>
